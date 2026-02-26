191a4ea788c8c27de0f3ebe9128ea598cbfe4d67
alexfilutze-byte <alexfilutze@gmail.com>
2026-02-25 23:24:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
